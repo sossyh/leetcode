4e302ef0c287a5abc652d332830d6d00f9e9a62f
sossyh <88205224+sossyh@users.noreply.github.com>
2022-07-06 15:08:49 +0300

    Update 50-powx-n.py

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,13 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        # if(n>=1):
-        #     if(n==1):
-        #         return x
-        #     else:
-        #         return x*self.myPow(x,n-1)
-        # else:
-        #     if(n==0):
-        #         return 1
-        #     else:
-        #         return (1/x)*self.myPow(x,n+1)
-        return math.pow(x,n)
+         if(n>=1):
+             if(n==1):
+                 return x
+             else:
+                 return x*self.myPow(x,n-1)
+         else:
+             if(n==0):
+                 return 1
+             else:
+                 return (1/x)*self.myPow(x,n+1)
+        
