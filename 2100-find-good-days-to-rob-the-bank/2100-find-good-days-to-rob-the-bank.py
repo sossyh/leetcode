dea8a4f46ec64493cde2c07a26c3edab603bee0a
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2022-10-19 10:26:08 +0300

    Time: 2326 ms (46.03%), Space: 33.4 MB (39.96%) - LeetHub

---
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
@@ -0,0 +1,28 @@
+class Solution:
+    def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
+        result=[]
+        decreasing = [1] * len(security)
+        
+        count = 0
+        for i in range(1, len(security)):
+            if security[i] <= security[i - 1]:
+                count += 1
+            else:
+                count = 0
+            decreasing[i] = count
+        
+        increasing = [0] * len(security)
+        increasing[-1] = 1
+        count = 0
+        for i in range(len(security) - 2 , -1, -1):
+            if security[i] <= security[i+1]:
+                count += 1
+            else:
+                count = 0
+            increasing[i] = count
+        
+        
+        for i in range(time, len(security) - time):
+            if increasing[i] >= time and decreasing[i] >= time:
+                result.append(i)
+        return result
